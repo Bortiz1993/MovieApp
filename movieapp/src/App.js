3432b2eb0d31b6fdf12a960a84bbeb540b04659d
Bortiz1993 <brijido.zuniga@bryanuniversity.edu>
2022-06-25 00:23:41 -0500

    update1

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -12,6 +12,7 @@ import {
 } from "@appbaseio/reactivesearch";
 import './App.css';
 
+//initializing state, it contains a clicked boolean and a variable.
 class App extends Component {
   constructor(props){
     super(props);
@@ -21,7 +22,42 @@ class App extends Component {
       message: "🔬Show Filters",
     }
   }
+
+  handleClick() {
+    this.setState({
+      isClicked: !this.state.isClicked,
+      message: this.state.isClicked ? "Show Filters" : "Show Movies",
+
+    });
+  }
   
+  render(){
+    return(
+      <div className="main-container">
+      <ReactiveBase
+      app="movies-demo-app"
+      url="https://81719ecd9552:e06db001-a6d8-4cc2-bc43-9c15b1c0c987@appbase-demo-ansible-abxiydt-arc.searchbase.io"
+      //stylings for the main container.
+      enableAppbase
+      theme={{
+        typography: {
+          fontFamily:
+          '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Noto Sans", "Ubuntu", "Droid Sans", "Helvetica Neue", sans-serif',
+          fontSize: "16px",
+        },
+        colors: {
+          backgroundColor: "#212121",
+          primaryTextColor: "#fff",
+          primaryColor: "#2196F3",
+          titleColor: "#fff",
+          alertColor: "#d9534f",
+          borderColor: "#666",
+        },
+      }}
+      ></ReactiveBase>
+      </div>
+    )
+  }
 }
 
 export default App;
